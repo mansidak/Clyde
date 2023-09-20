fcea17ef187221ac465e7c3d8305744adb311c11
Mansidak Singh <mansidaknoor@gmail.com>
2023-09-20 18:08:42 -0500

    Initial Commit

---
+++ Sudo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sudo
+//
+//  Created by Mansidak Singh on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sudo/SudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudoApp.swift
+//  Sudo
+//
+//  Created by Mansidak Singh on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
